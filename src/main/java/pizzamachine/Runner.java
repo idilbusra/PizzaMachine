43996f2556ba54def12b04f741e35e215e2bf218
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-04 23:36:23 +0300

    V2

--- src/main/java/pizzamachine/Runner.java
+++ src/main/java/pizzamachine/Runner.java
@@ -2,7 +2,6 @@ package pizzamachine;
 
 public class Runner {
     public static void main(String[] args) {
-        PizzaMachine  pizzaMachine = new PizzaMachine();
         PizzaMachine.boyutSec();
         PizzaMachine.hamurTipiSec();
         PizzaMachine.malzemeEkle();
